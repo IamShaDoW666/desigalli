d450e6475dbdb3ebec175f33860bdb2e020b8bf9
nibhapr <nibhapr@gmail.com>
2022-05-15 21:43:52 +0530

    edit settings

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -199,7 +199,11 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
