727181e4fc9964ef6f0bed4c737672aeca43a95d
nibhapr <nibhapr@gmail.com>
2022-05-16 00:06:30 +0530

    added ip

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -58,7 +58,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   let updateToken = async () => {
-    let response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
+    let response = await fetch("http://164.92.165.54/api/token/refresh/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
