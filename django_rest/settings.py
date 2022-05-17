434325ad845421733fc040138ff76e6381aa13d7
IamShaDoW666 <www.gaming2018@gmail.com>
2022-05-17 23:35:36 +0530

    small fix

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -31,9 +31,8 @@ SECRET_KEY = 'django-insecure-&y1s46me%82x8)1wq=luw97wg61!6(i^^ez3=*3cop+ljh+he2
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# print(env('APP_IP'))
-# raise SystemExit
-ALLOWED_HOSTS = [env('APP_IP')]
+
+ALLOWED_HOSTS = ['139.59.140.77']
 
 
 # Application definition
@@ -160,15 +159,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOWED_ORIGINS = [
-    env('APP_URL')   
+    'http://139.59.140.77'   
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    env('APP_URL')
+    'http://139.59.140.77'
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    env('APP_URL')+'/login',
+    'http://139.59.140.77/login',
 ]
 CORS_ALLOW_METHODS = [
     "DELETE",
